39a27f0d0b5f77da5df6117556110576dd9d0409
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-01-09 12:11:54 +0700

    feat: add requests

---
+++ app/Http/Requests/Role/UpdateRoleRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Role;
+
+use App\Models\ManagementAccess\Role;
+// use Illuminate\Auth\Access\Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+class UpdateRoleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => [
+                'required', 'string', 'max:255',
+            ],
+            // add validation for role this here
+        ];
+    }
+}
